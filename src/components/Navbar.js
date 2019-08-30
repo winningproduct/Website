cbd31d0000f27ca98bb19a9e4db0c099c74428da
nir099 <a.m.s.nirmal.a@gmail.com>
2019-08-30 12:13:57 +0530

    removing product , contact pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -63,17 +63,8 @@ const Navbar = class extends React.Component {
               <Link className="navbar-item" to="/about">
                 About
               </Link>
-              <Link className="navbar-item" to="/products">
-                Products
-              </Link>
               <Link className="navbar-item" to="/blog">
                 Blog
-              </Link>
-              <Link className="navbar-item" to="/contact">
-                Contact
-              </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
               </Link>
             </div>
             <div className="navbar-end has-text-centered">
