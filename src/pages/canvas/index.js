5f40a54bad4c2fe9eaca67197f44f7558f3c02dc
nir099 <a.m.s.nirmal.a@gmail.com>
2019-09-11 17:22:02 +0530

    added side bar

--- src/pages/canvas/index.js
+++ src/pages/canvas/index.js
@@ -20,146 +20,18 @@ let allLinks = {}
 let bigNode = {};
 let linkCount = 0;
 
-let color = (type) => {
-    switch (type) {
-        case '1-userExperience':
-            return 'rgb(255, 171, 64)';
-        case '2-marketSense':
-            return 'rgb(142, 124, 195)';
-        case '4-customerSuccess':
-            return 'rgb(109, 158, 235)';
-        case '3-technologyExcellence':
-            return 'rgb(106, 168, 79)';
-        default:
-            return '#9a8d8d1a';
-    }
-}
-
-let xPos = (type) => {
-    switch (type) {
-        case '1-userExperience':
-            return 0;
-        case '2-marketSense':
-            return 300;
-        case '4-customerSuccess':
-            return 700;
-        case '3-technologyExcellence':
-            return 1100;
-        default:
-            return null;
-    }
-}
-
-let yPos = (type) => {
-    switch (type) {
-        case '1-Explore':
-            return { y: 0 };
-        case '2-Focus':
-            return { y: 800 };
-        case '3-immerse':
-            return { y: 1850 };
-        case '4-Plan':
-            return { y: 3050 };
-        case '5-Build':
-            return { y: 4700 };
-        case '6-Stabilize':
-            return { y: 6950 };
-        case '7-Optimize':
-            return { y: 8900 };
-        case '8-Harvest':
-            return { y: 10700 };
-        case '9-Retire':
-            return { y: 11800 };
-        default:
-            return null;
-    }
-}
-
-nodes.sort((current, next) => (current.group > next.group) ? 1 : (current.group === next.group) ? ((current.type > next.type) ? 1 : ((current.order > next.order) ? 1 : -1)) : -1);
-
-
-nodes.map((node, i) => {
-
-    // node structure
-    let colorType = color(node.type);
-    let pos = yPos(node.group);
-    let ports = {}
-
-    let out = {
-        port1: {
-            id: "port1",
-            type: "output"
-        }
-    }
-    ports = Object.assign(ports, out);
-
-    let input = {
-        port2: {
-            id: "port2",
-            type: "input"
-        }
-    }
-    ports = Object.assign(ports, input);
-
-    nody = {
-        id: node.id,
-        title: node.title,
-        color: colorType,
-        type: "default",
-        url: node.url,
-        position: {
-            x: 50 + xPos(node.type) + node.order * 10,
-            y: node.order * 150 + pos.y
-        },
-        ports: ports
-    };
-
-    bigNode[`${node.id}`] = nody;
-
-    // link structure
-    node.to.map((link, i) => {
-        let linkd = {
-            id: `link${linkCount + 1}`,
-            from: {
-                nodeId: node.id,
-                portId: "port1"
-            },
-            to: {
-                nodeId: link,
-                portId: "port2"
-            }
-        }
-        linkCount++;
-        allLinks[`link${linkCount}`] = linkd;
-        return true;
-    });
-    return true;
-});
-
-const complexChart = {
-    offset: {
-        x: 0,
-        y: 0
-    },
-    nodes: bigNode,
-    links: allLinks,
-    selected: {},
-    hovered: {}
-}
-
-
 const Outer = styled.div`
     padding: 30px;
     border-radius: 4px;
     box-shadow: 0px 0px 0px 0px rgba(239,235,233,0), 
-        10px -10px 20px -4px rgba(144,148,154,0.38);
+        10px -12px 20px -4px rgba(144,148,154,0.38);
 `
 const CanvasOuterCustom = styled.div`
   text-align: center
   position: relative;
   background-size: 10px 10px;
   background-color: #fff;
-  width: 100%;
+  width: ${window.innerWidth * 0.8}px;
   height: 13500px;
   overflow-x: scroll;
   cursor: not-allowed;
@@ -171,9 +43,9 @@ const CanvasOuterCustom = styled.div`
       top 3100px left 0px, //plan
       top 4750px left 0px, //build
       top 6950px left 0px, //stabilize
-      top 8950px left 0px,
-      top 10800px left 0px,
-      top 12000px left 0px;
+      top 8950px left 0px, // optimize
+      top 10800px left 0px, // harvest
+      top 12000px left 0px; // retier
   background-repeat: no-repeat;
   background-size: 
     100% 800px ,  // explore
@@ -183,8 +55,8 @@ const CanvasOuterCustom = styled.div`
     100% 2350px, // build
     100% 2100px, // stabilize
     100% 1900px, // optimize
-    100% 1300px,
-    100% 1700px;
+    100% 1300px, // harvest
+    100% 1700px; // retier
   background-image:
     url(${explore}),
     url(${focus}),
@@ -206,7 +78,7 @@ cursor: move;
 const NodeInnerCustom = ({ node }) => {
     if (node.type) {
         return (
-            <Outer style={{background: `linear-gradient(to bottom , ${node.color} , white )`}}>
+            <Outer style={{ background: `linear-gradient(to bottom , ${node.color} , white )` }}>
                 {node.id.replace(/_/g, ' ')}
                 <br />
                 <a href={node.url}>Link</a>
@@ -216,17 +88,256 @@ const NodeInnerCustom = ({ node }) => {
 }
 
 export default class CanvasIndexPage extends React.Component {
+    state = {
+        windowWidth: 1280
+    }
+
+    handleResize = () => this.setState({
+        windowWidth: window.innerWidth
+    });
+
+    componentDidMount() {
+        this.handleResize();
+        window.addEventListener('resize', this.handleResize)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleResize)
+    }
+
+    color = (type) => {
+        switch (type) {
+            case '1-userExperience':
+                return 'rgb(255, 171, 64)';
+            case '2-marketSense':
+                return 'rgb(142, 124, 195)';
+            case '4-customerSuccess':
+                return 'rgb(109, 158, 235)';
+            case '3-technologyExcellence':
+                return 'rgb(106, 168, 79)';
+            default:
+                return '#9a8d8d1a';
+        }
+    }
+
+    xPos = (type) => {
+        switch (type) {
+            case '1-userExperience':
+                return 0;
+            case '2-marketSense':
+                return this.state.windowWidth > 600 ? this.state.windowWidth * 0.1 : 200;
+            case '4-customerSuccess':
+                return this.state.windowWidth > 600 ? this.state.windowWidth * 0.3 : 500;
+            case '3-technologyExcellence':
+                return this.state.windowWidth > 600 ? this.state.windowWidth * 0.5 : 700;
+            default:
+                return null;
+        }
+    }
+
+    yPos = (type) => {
+        switch (type) {
+            case '1-Explore':
+                return { y: 0 };
+            case '2-Focus':
+                return { y: 800 };
+            case '3-immerse':
+                return { y: 1850 };
+            case '4-Plan':
+                return { y: 3050 };
+            case '5-Build':
+                return { y: 4700 };
+            case '6-Stabilize':
+                return { y: 6950 };
+            case '7-Optimize':
+                return { y: 8900 };
+            case '8-Harvest':
+                return { y: 10700 };
+            case '9-Retire':
+                return { y: 11800 };
+            default:
+                return null;
+        }
+    }
+
+    complexChart = () => {
+
+        nodes.sort((current, next) => (current.group > next.group) ? 1 : (current.group === next.group) ? ((current.type > next.type) ? 1 : ((current.order > next.order) ? 1 : -1)) : -1);
+
+        nodes.map((node, i) => {
+
+            // node structure
+            let colorType = this.color(node.type);
+            let pos = this.yPos(node.group);
+            let ports = {}
+
+            let out = {
+                port1: {
+                    id: "port1",
+                    type: "output"
+                }
+            }
+            ports = Object.assign(ports, out);
+
+            let input = {
+                port2: {
+                    id: "port2",
+                    type: "input"
+                }
+            }
+            ports = Object.assign(ports, input);
+
+            nody = {
+                id: node.id,
+                title: node.title,
+                color: colorType,
+                type: "default",
+                url: node.url,
+                position: {
+                    x: 10 + this.xPos(node.type) + node.order * 10,
+                    y: node.order * 150 + pos.y
+                },
+                ports: ports
+            };
+
+            bigNode[`${node.id}`] = nody;
+
+            // link structure
+            node.to.map((link, i) => {
+                let linkd = {
+                    id: `link${linkCount + 1}`,
+                    from: {
+                        nodeId: node.id,
+                        portId: "port1"
+                    },
+                    to: {
+                        nodeId: link,
+                        portId: "port2"
+                    }
+                }
+                linkCount++;
+                allLinks[`link${linkCount}`] = linkd;
+                return true;
+            });
+            return true;
+        });
+
+        return {
+            offset: {
+                x: 0,
+                y: 0
+            },
+            nodes: bigNode,
+            links: allLinks,
+            selected: {},
+            hovered: {}
+        }
+    }
     render() {
         return (
             <Layout>
-                <div>
-                    <FlowChartWithState config={{ readonly: true }} Components={{
-                        NodeInner: NodeInnerCustom,
-                        CanvasOuter: CanvasOuterCustom,
-                        CanvasInner: CanvasInnerCustom
-                    }} initialValue={complexChart} />
+                <div style={style.row}  >
+                    <div style={style.left} >
+                        <FlowChartWithState config={{ readonly: true }} Components={{
+                            NodeInner: NodeInnerCustom,
+                            CanvasOuter: CanvasOuterCustom,
+                            CanvasInner: CanvasInnerCustom
+                        }} initialValue={this.complexChart()} />
+                    </div>
+                    <br style={{ "clear": "both" }} />
+                    <div style={style.right}>
+                        <div style={style.contentExplore}>
+                            <span style={style.text}>Explore</span>
+                        </div>
+                        <div style={style.contentFocus}>
+                            <span style={style.text}>Focus</span>
+                        </div>
+                        <div style={style.contentImmerse}>
+                            <span style={style.text}>Immerse</span>
+                        </div>
+                        <div style={style.contentPlan}>
+                            <span style={style.text}>Plan</span>
+                        </div>
+                        <div style={style.contentBuild}>
+                            <span style={style.text}>Build</span>
+                        </div>
+                        <div style={style.contentStabilize}>
+                            <span style={style.text}>Stabilize</span>
+                        </div>
+                        <div style={style.contentOptimize}>
+                            <span style={style.text}>Optimize</span>
+                        </div>
+                        <div style={style.contentHarvest}>
+                            <span style={style.text}>Harvest</span>
+                        </div>
+                        <div style={style.contentRetier}>
+                            <span style={style.text}>Retier</span>
+                        </div>
+                    </div>
                 </div>
             </Layout>
         )
+    }
+}
+
+const style = {
+    row: {
+        display: "flex"
+    },
+    left: {
+        width: window.innerWidth * 0.8
+    },
+    right: {
+        width: window.innerWidth * 0.2
+    },
+    contentExplore: {
+        height: "800px",
+        textAlign: "center",
+        lineHeight: "800px"
+    },
+    contentFocus: {
+        height: "1050px",
+        textAlign: "center",
+        lineHeight: "1050px"
+    },
+    contentImmerse: {
+        height: "1250px",
+        textAlign: "center",
+        lineHeight: "1250px"
+    },
+    contentPlan: {
+        height: "1750px",
+        textAlign: "center",
+        lineHeight: "1750px"
+    },
+    contentBuild: {
+        height: "2350px",
+        textAlign: "center",
+        lineHeight: "2350px"
+    },
+    contentStabilize: {
+        height: "2100px",
+        textAlign: "center",
+        lineHeight: "2100px"
+    },
+    contentOptimize: {
+        height: "1900px",
+        textAlign: "center",
+        lineHeight: "1900px"
+    },
+    contentHarvest: {
+        height: "1300px",
+        textAlign: "center",
+        lineHeight: "1300px"
+    },
+    contentRetier: {
+        height: "1700px",
+        textAlign: "center",
+        lineHeight: "1700px"
+    },
+    text: {
+        fontFamily: "sans-serif",
+        fontSize: window.innerWidth * 0.2 * 0.15,
+        fontWeight: "600"
     }
 }
