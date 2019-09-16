02dc6afff5af8dc86e2aea1a1f0904925f92f63c
nir099 <a.m.s.nirmal.a@gmail.com>
2019-09-16 15:08:53 +0530

    refactoring

--- src/pages/canvas/index.js
+++ src/pages/canvas/index.js
@@ -1,27 +1,16 @@
 import React from 'react';
 import Layout from '../../components/Layout'
 import { FlowChartWithState } from "@mrblenny/react-flow-chart";
-import styled from 'styled-components'
 import data from "./data.json";
 import { Helmet } from 'react-helmet'
+import './canvas.css'
+import { CanvasInnerCustom, Outer, CanvasOuterCustom} from './styleComponents'
 
 let nodes = data.nodes;
 let singleNode = {}
 let allLinks = {}
 let bigNode = {};
 let linkCount = 0;
-
-const Outer = styled.div`
-    padding: 30px;
-    border-radius: 4px;
-    box-shadow: 0px 0px 0px 0px rgba(239,235,233,0), 
-    10px -12px 20px -4px rgba(144,148,154,0.38);
-    `
-const CanvasInnerCustom = styled.div`
-    width: 100%;
-    position: relative;
-    cursor: move;
-`
 
 const NodeInnerCustom = ({ node }) => {
     if (node.type) {
@@ -34,7 +23,6 @@ const NodeInnerCustom = ({ node }) => {
         )
     }
 }
-
 export default class CanvasIndexPage extends React.Component {
     state = {
         windowWidth: 1920
@@ -186,10 +174,12 @@ export default class CanvasIndexPage extends React.Component {
 
 
     render() {
-
         let style = {
             row: {
                 display: "flex"
+            },
+            path: {
+                stroke: "black !important"
             },
             left: {
                 width: this.state.windowWidth * 0.8
@@ -248,43 +238,6 @@ export default class CanvasIndexPage extends React.Component {
                 transform: "translateY(-50%)"
             }
         }
-
-        let CanvasOuterCustom = styled.div`
-            text-align: center
-            position: relative;
-            background-size: 10px 10px;
-            background-color: #fff;
-            width: auto;
-            height: 13500px;
-            overflow-x: scroll;
-            cursor: not-allowed;
-            outline: none !important;
-            background-repeat: no-repeat;
-            background: 
-                linear-gradient(
-                    180deg,
-                    #fff 0px,
-                    #fff 950px, 
-                    #f3f3f3 950px,
-                    #f3f3f3 2000px, 
-                    #fff 2000px,
-                    #fff 3200px, 
-                    #f3f3f3 3200px,
-                    #f3f3f3 4850px, 
-                    #fff 4850px,
-                    #fff 7100px,
-                    #f3f3f3 7100px,
-                    #f3f3f3 9050px,
-                    #fff 9050px,
-                    #fff 10850px,
-                    #f3f3f3 10850px,
-                    #f3f3f3 11950px,
-                    #fff 11950px
-                    );
-    `
-
-
-
         return (
             <Layout>
                 <div>
