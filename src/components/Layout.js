15a04f1c260f928fc99c2a281048e68f00a25bd0
nir099 <a.m.s.nirmal.a@gmail.com>
2019-11-04 12:47:08 +0530

    testing css fix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
-import './all.sass'
 import useSiteMetadata from './SiteMetadata'
 import { withPrefix } from "gatsby"
+require('./all.sass');
 
 const TemplateWrapper = ({ children }) => {
   const { title, description } = useSiteMetadata()
